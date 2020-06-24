7cf53f13d3c72cd75c6a3f288db07cb2bfb32b08
Brandon Gallagher <brandonegallagher@gmail.com>
2020-06-24 18:23:23 -0400

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import {Avatar} from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.default}>
+      <Text>TEST 123</Text>
+      <View style ={ styles.box }>
+      </View>
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
+
+  default : {
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  box: {
+    width : "100px",
+    height : "100px",
+    borderColor : "red",
+    borderWidth: "10px"
+  }
 });
